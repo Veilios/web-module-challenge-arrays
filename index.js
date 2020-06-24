8f201245c40f0e855f092f2f9ec2a9dfbde2678a
Alan Paul Mir <alanpaulphotos@gmail.com>
2020-06-23 22:14:09 -0400

    Made a function for task 7, that so far only checks for elements in the array with the word chocolate and returns true or false

--- index.js
+++ index.js
@@ -136,8 +136,8 @@ function copy(iceCreamCopy, iceCream){
     console.log(iceCream);
 }
 
-let ogFlavors = []
-copy(ogFlavors, originalFlavors);
+// let ogFlavors = [];
+// copy(ogFlavors, originalFlavors);
 
 /* Task 7: July 7th is "World Chocolate Day" and Baskin Robins wants to create promotional materials highlighting all of their chocolate flavors. Write a function that checks every item in the array for a given string and returns a new array called filteredArray with just these values. Rather than hardcoding "chocolate" into your function, pass a string as a parameter, and invoke with the argument "chocolate". This way you could also filter for "Vanilla", "Sherbert", etc. when those holidays roll around.
 
@@ -154,13 +154,16 @@ DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem.
 
 hint - you can use the .includes method to help you solve this */
 
-function filterByWord(/*code here*/){
-
-    /*code here*/
-
+function filterByWord(iceCream, flavor){  
+    for(i = 0; i < iceCream.length; i++){
+        if(iceCream[i].includes("Chocolate") === true){
+            
+        }
+    }
+    console.log(holiday);
 }
-
-
+let holiday = [];
+filterByWord(originalFlavors, "Chocolate");
 
 /* 🧁🍦🍨 STRETCH 🍨🍦🍫*/ 
 
